3cfb7f147f52b2edd418d4252f100b2ee614bd8b
Mattéo DETALLE <matteo.detalle@student.junia.com>
2025-09-09 13:43:27 +0200

    exo 4

--- TP1/TP1_4/TP1_4.c
+++ TP1/TP1_4/TP1_4.c
@@ -2,16 +2,31 @@
 #include <stdlib.h>
 
 int factoriellefor(int chiffre){
-
+	int result = 1;
+	for (int i = chiffre; i > 0; i -= 1) {
+		result = result * i;
+	}
+	return result;
 }
 
 int factoriellewhile(int chiffre) {
-
+	int result = 1;
+	while (chiffre > 0) {
+		result = result * chiffre;
+		chiffre -= 1;
+	}
+	return result;
 }
 
 int factoriellerec(int chiffre) {
-
+	if (chiffre == 0) {
+		return chiffre + 1;
+	}
+	return factoriellerec(chiffre - 1) * chiffre;
 }
 int main() {
+	printf("%d\n", factoriellefor(5));
+	printf("%d\n", factoriellewhile(5));
+	printf("%d\n", factoriellerec(5));
 	return EXIT_SUCCESS;
 }
