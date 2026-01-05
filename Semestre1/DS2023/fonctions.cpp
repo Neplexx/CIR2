d58e674dad2c7125947662e82c58ce1aa15386ce
Mattéo DETALLE <matteo.detalle@student.junia.com>
2026-01-05 19:41:00 +0100

    fin Semestre 1

--- Semestre1/DS2023/fonctions.cpp
+++ Semestre1/DS2023/fonctions.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include<algorithm>
 #include "fonctions.hpp"
 
 int* aleatoire(int nombre) {
 	if (nombre <= 0) {
 		return 0;
 	}
-	int* tab = new int[nombre]();
+	std::vector<int> tableau;
+	int* tab = new int[nombre];
 	for (int i = 0; i < nombre; i += 1) {
 		tab[i] = rand();
 	}
+	std::sort(tab[0], tab[nombre]);
+	std::sort(tableau.begin(), tableau.end());
 	return tab;
 }
 
 int Polynome::max_degree() {
     return 0;
 }
+Polynome::Polynome(const Polynome& poly) : polynome_(poly.polynome_) {}
+
