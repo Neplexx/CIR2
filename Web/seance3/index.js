99ab5ccb7374a0edd4218e20e7a58ba78b527993
Mattéo DETALLE <matteo.detalle@student.junia.com>
2026-02-04 15:30:32 +0100

    fin cours

--- Web/seance3/index.js
+++ Web/seance3/index.js
@@ -10,3 +10,11 @@ let span = document.getElementsByClassName('highlight');
 console.log(span);
 
 span[0].textContent = "New Hightlighted Text"
+
+let paragraph = document.createElement("li");
+paragraph.innerText = "New Paragraph";
+let container = document.querySelector('ul#listContainer');
+container.appendChild(paragraph);
+
+let remplacement = document.getElementById("listContainer");
+remplacement.firstElementChild.innerText = "Replaced Paragraph";
